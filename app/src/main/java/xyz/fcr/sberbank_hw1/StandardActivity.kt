f4bb5f48c2b00a26843d4bf9112f64bbce7b4c86
Oleg Ryabyshev <oryabyshev@gmail.com>
2021-06-25 20:13:15 +0300

    Lesson 2: Tasks

---
+++ app/src/main/java/xyz/fcr/sberbank_hw1/StandardActivity.kt
@@ -0,0 +1,68 @@
+package xyz.fcr.sberbank_hw1
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+
+private var number: Int = 0
+private const val NUMBER_TAG = "Number tag"
+
+class StandardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity)
+
+        number = intent.getIntExtra(NUMBER_TAG, 0)
+
+        //Setting up text views
+        val nameTextView: TextView = findViewById(R.id.name)
+        nameTextView.text = getString(R.string.standard)
+
+        val numberTextView: TextView = findViewById(R.id.number)
+        numberTextView.text = number++.toString()
+
+
+        //Setting up buttons
+        val buttonStandard: MaterialButton = findViewById(R.id.button_standard)
+        val buttonSingleTop: MaterialButton = findViewById(R.id.button_single_top)
+        val buttonSingleTask: MaterialButton = findViewById(R.id.button_single_task)
+        val buttonSingleInstance: MaterialButton = findViewById(R.id.button_single_instance)
+
+        buttonStandard.setOnClickListener {
+            val intent = Intent(this, StandardActivity::class.java).apply {
+                putExtra(NUMBER_TAG, number)
+            }
+            startActivity(intent)
+        }
+
+        buttonSingleTop.setOnClickListener {
+            val intent = Intent(this, SingleTopActivity::class.java).apply {
+                putExtra(NUMBER_TAG, number)
+            }
+            startActivity(intent)
+        }
+
+        buttonSingleTask.setOnClickListener {
+            val intent = Intent(this, SingleTaskActivity::class.java).apply {
+                putExtra(NUMBER_TAG, number)
+            }
+            startActivity(intent)
+        }
+
+        buttonSingleInstance.setOnClickListener {
+            val intent = Intent(this, SingleInstanceActivity::class.java).apply {
+                putExtra(NUMBER_TAG, number)
+            }
+            startActivity(intent)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        val numberTextView: TextView = findViewById(R.id.number)
+        number = numberTextView.text.toString().toInt()
+        number++
+    }
+}
